a69821b18125453ce8d25b06d7366ff6393e0a73
rizqisamawa <rizqisamawa11@gmail.com>
2022-07-25 00:32:40 +0700

    cleaning code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,11 @@
-import React from "react";
 import Popular from "../components/Popular";
 import Veggie from "../components/Veggie";
 
 function Home() {
   return (
     <>
-      <Popular />
       <Veggie />
+      <Popular />
     </>
   );
 }
