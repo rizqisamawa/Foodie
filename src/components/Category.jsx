032a28ac1e1269a091e505996d711fd0d7bd9ce7
rizqisamawa <rizqisamawa11@gmail.com>
2022-08-04 23:22:02 +0700

    add navlink and style

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -6,22 +6,22 @@ import styled from "styled-components";
 function Category() {
   return (
     <List>
-      <NavLink to={"/cuisine/Italian"}>
+      <Slink to={"/cuisine/Italian"}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/American"}>
+      </Slink>
+      <Slink to={"/cuisine/American"}>
         <FaHamburger />
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/Thailand"}>
+      </Slink>
+      <Slink to={"/cuisine/Thai"}>
         <GiNoodles />
         <h4>Thailand</h4>
-      </NavLink>
-      <NavLink to={"/cuisine/Japanese"}>
+      </Slink>
+      <Slink to={"/cuisine/Japanese"}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </NavLink>
+      </Slink>
     </List>
   );
 }
@@ -31,6 +31,40 @@ const List = styled.div`
   justify-content: center;
   margin: 2rem 0rem;
   text-align: center;
+`;
+
+const Slink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient(35deg, #494949, #313131);
+  width: 6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4 {
+    color: white;
+    font-size: 0.8rem;
+  }
+  svg {
+    color: white;
+    font-size: 1.5rem;
+  }
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    svg {
+      color: white;
+    }
+    h4 {
+      color: white;
+    }
+  }
 `;
 
 export default Category;
