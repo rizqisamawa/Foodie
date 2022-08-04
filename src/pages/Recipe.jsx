13a243dfd682432fc97587cc8b8a699cfaf9a824
rizqisamawa <rizqisamawa11@gmail.com>
2022-08-05 00:06:17 +0700

    new file recipe page

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,9 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+function Recipe() {
+  return <div>Recipe</div>;
+}
+
+export default Recipe;
