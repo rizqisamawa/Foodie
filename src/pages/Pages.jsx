ef8503bac575c090d1652a953ceec285d9eec8b9
rizqisamawa <rizqisamawa11@gmail.com>
2022-08-04 11:43:30 +0700

    add react-router

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,16 +1,13 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 
 function Pages() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cuisine" element={<Cuisine />} />
-        {/* <Route path="/" element={<Home />} /> */}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cuisine/:type" element={<Cuisine />} />
+    </Routes>
   );
 }
 
